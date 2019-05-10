fa079af6aa68f325f40e66e4f3d471208f461769
Chailloux Mathieu <mathieu@chailloux.org>
2019-05-10 14:47:40 +0200

    Header for styles.py

--- styles.py
+++ styles.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ qgis-lib-mc
+ PyQGIS utilities library to develop plugins or scripts
+                             -------------------
+        begin                : 2019-02-21
+        author               : Mathieu Chailloux
+        email                : mathieu.chailloux@irstea.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+"""
+    Useful functions to perform rendering style operations.
+"""
 
 import random
 
